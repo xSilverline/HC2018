8835f4d167149703bca3833f23083631e0a17fd4
xSilverline <32816233+xSilverline@users.noreply.github.com>
2018-03-01 19:40:37 +0100

    Added Car class

---
+++ src/main/HashCode/Car.java
@@ -0,0 +1,49 @@
+
+
+public class Car
+{
+    private int startInsertionX;
+    private int startInsertionY;
+    private int finishInsertionX;
+    private int finishInsertionY;
+    private int start;
+    private int finish;
+
+    public Car(int a, int b, int x, int y, int s, int f)
+    {
+        this.startInsertionX = a;
+        this.startInsertionY = b;
+        this.finishInsertionX = x;
+        this.finishInsertionY = y;
+        this.start = s;
+        this.finish = f;
+    }
+
+    int getStart()
+    {
+        return start;
+    }
+    int getFinish()
+    {
+        return finish;
+    }
+
+    int getStartInsertionX()
+    {
+        return startInsertionX;
+    }
+    int getStartInsertionY()
+    {
+        return startInsertionY;
+    }
+    int getFinishInsertionX()
+    {
+        return finishInsertionX;
+    }
+    int getFinishInsertionY()
+    {
+        return finishInsertionY;
+    }
+
+
+}
