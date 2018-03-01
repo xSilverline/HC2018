5893d02880ca99ed4ba88faabfbe7a756fb88785
Jonasz <jonaszwiacek@gmail.com>
2018-03-01 19:22:13 +0100

    input

--- src/main/java/InputContainer.java
+++ src/main/java/InputContainer.java
@@ -1,18 +1,46 @@
 package java;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputContainer {
+    List<Ride> rides = new ArrayList<>();
+    int vehicles;
+    int rows;
+    int columns;
+    int num_of_rides;
+    int bonus;
+    int steps;
 
-    public static void loadInput(String fileName) {
+    public void loadInput(String fileName) {
         try {
-            String line;
-
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            while((line = bufferedReader.readLine()) != null) {
 
+            String line = bufferedReader.readLine();
+            String[] params = line.split(" ");
+
+            rows = Integer.parseInt(params[0]);
+            columns = Integer.parseInt(params[1]);
+            vehicles = Integer.parseInt(params[2]);
+            num_of_rides = Integer.parseInt(params[3]);
+            bonus = Integer.parseInt(params[4]);
+            steps = Integer.parseInt(params[5]);
+
+            Ride r;
+            while((line = bufferedReader.readLine()) != null) {
+                String[] ride = line.split(" ");
+                r = new Ride(
+                        Integer.parseInt(ride[0]), //start x
+                        Integer.parseInt(ride[1]), //start y
+                        Integer.parseInt(ride[2]), //end x
+                        Integer.parseInt(ride[3]), //end y
+                        Integer.parseInt(ride[4]), //start
+                        Integer.parseInt(ride[5]) //end
+                        );
+                rides.add(r);
             }
 
         } catch (FileNotFoundException e) {
@@ -22,5 +50,10 @@ public class InputContainer {
         } finally {
 
         }
+    }
+
+    public static void main(String[] args) {
+        InputContainer inputContainer = new InputContainer();
+        inputContainer.loadInput("");
     }
 }
